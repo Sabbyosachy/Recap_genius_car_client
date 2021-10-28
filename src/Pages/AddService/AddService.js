01036fcc2f227b5c50513aa48ea5e5696f9f0536
Sabbyosachy <subbyosachychakraborty@gmail.com>
2021-10-28 14:49:17 +0600

    final push

---
+++ src/Pages/AddService/AddService.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import React from 'react';
+import { useForm } from "react-hook-form";
+import './AddService.css';
+
+const AddService = () => {
+    const { register, handleSubmit,reset } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+    axios.post('http://localhost:5000/allservices',data)
+    .then(res=>{
+        if(res.data.insertedId){
+            alert('Added Succefully')
+            reset();
+        }
+    })
+    
+  }
+    return (
+        <div className="added_form">
+            <h3>Add New Services</h3>
+            <form onSubmit={handleSubmit(onSubmit)}>
+      <input placeholder="Name" type="text" {...register("name")} />
+      <input placeholder="Description" type="text" {...register("description")} />
+      <input placeholder="Price" type="number" {...register("price")} />
+      <input placeholder="Image"{...register("img")} />
+
+      <input className="btn" type="submit" />
+    </form>
+            
+        </div>
+    );
+};
+
+export default AddService;
--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import './Booking.css';
 
 const Booking = () => {
     const { serviceId } = useParams();
+    const[service,setService]=useState({});
+    useEffect(()=>{
+        fetch(`http://localhost:5000/allservices/${serviceId}`)
+        .then(res=>res.json())
+        .then(data=>setService(data))
+    },[])
     return (
-        <div>
-            <h2>this is booking: {serviceId}</h2>
+        <div className="singleService">
+            <img src={service.img} alt="" srcset="" />
+            <h3>{service.name}</h3>
+            <p>{service.description}</p>
+            <h6>Price: {service.price}</h6>
         </div>
     );
 };
